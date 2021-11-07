8f90f562cfcd364bd7263ec94a2b29ab97645533
Lee <lee_booth@live.co.uk>
2021-11-07 22:25:16 +0000

    Update player colour to match objects we hit

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] float destroyDelay = 0.5f;
+    bool hasPackage = false;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void OnCollisionEnter2D(Collision2D other) {
-        Debug.Log("Collided with: " + other.gameObject.name);
+        // Debug.Log("Collided with: " + other.gameObject.name);
         
     }
 
     void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("Collided with: " + other.gameObject.name);
-
-        if (other.tag == "Package"){
+        if (other.tag == "Package" && !hasPackage){
             Debug.Log("Hit a package");
-
+            hasPackage = true;
+            spriteRenderer.color = other.gameObject.GetComponent<SpriteRenderer>().color;
+            Destroy(other.gameObject, destroyDelay);
         }
-        else {
+        
+        if (other.tag == "Customer" && hasPackage) {
             Debug.Log("Hit a customer");
-
+            hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
         
     }
