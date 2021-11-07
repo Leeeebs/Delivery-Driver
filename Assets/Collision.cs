729c53fe599b6da1abe8602082582f7b857694e3
Lee <lee_booth@live.co.uk>
2021-11-07 20:36:53 +0000

    Added trigger example to collision script

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -8,4 +8,9 @@ public class Collision : MonoBehaviour
         Debug.Log("Collided with: " + other.gameObject.name);
         
     }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Collided with: " + other.gameObject.name);
+        
+    }
 }
