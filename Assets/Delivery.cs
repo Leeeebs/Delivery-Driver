87a08d22fef11fc6c91f2b989dce743a89523882
Lee <lee_booth@live.co.uk>
2021-11-07 21:49:54 +0000

    Added tags to certain game objects & updated collision script

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class Delivery : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Collided with: " + other.gameObject.name);
@@ -11,6 +11,15 @@ public class Collision : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other) {
         Debug.Log("Collided with: " + other.gameObject.name);
+
+        if (other.tag == "Package"){
+            Debug.Log("Hit a package");
+
+        }
+        else {
+            Debug.Log("Hit a customer");
+
+        }
         
     }
 }
